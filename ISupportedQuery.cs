1a64197e4caed4f2fc5fb16e244b08d1047214cb
Nick Smith <nsc319@gmail.com>
2022-04-11 17:26:55 -0400

    outline complete

---
+++ ISupportedQuery.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticDemo
+{
+    public abstract class ISupportedQuery
+    {
+        public string getName()
+        {
+            return this.GetType().Name;
+        }
+        public abstract string getQueryTerm();
+    }
+
+    public class SocialSecurityNumber : ISupportedQuery
+    {
+        public override string getQueryTerm()
+        {
+            return @"\b(?!000|666)[0-8][0-9]{2}-(?!00)[0-9]{2}-(?!0000)[0-9]{4}\b";
+        }
+    }
+
+    public class CreditCardNumber : ISupportedQuery
+    {
+        public override string getQueryTerm()
+        {
+            return @"^(?:(4[0-9]{12}(?:[0-9]{3})?)|(5[1-5][0-9]{14})|(6(?:011|5[0-9]{2})[0-9]{12})|(3[47][0-9]{13})|(3(?:0[0-5]|[68][0-9])[0-9]{11})|((?:2131|1800|35[0-9]{3})[0-9]{11}))$";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,54 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+
 using System.Linq;
 
 //began at 2:45 PM
 //dotnet 5.0 console project
 //github 
+//test data
 //starting with https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/how-to-query-the-contents-of-files-in-a-folder-lin
+//read into scalability concerns & wildcard possibilities of this impl
+//3:15 wanted to avoid regex for performance reasons, but that won't be possible so...
+// https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/how-to-combine-linq-queries-with-regular-expressions 
+//4:15 added classes & Regex for SSN & CCN 
+//Assumptions - have admin file/folder access. searching local folders/files, not over the network.
+//4:30 refactoring query & file loops. instead of per query per file, run per file per query using LINQ
+//5:00 LINQ/Regex issue getting the matched text back
+//5:15 commenting code, cleaning up
+//have not tested on 100k files, even so my first performance improvement would be to limit/batch the folders returned by getDirectories
 
 namespace GalacticDemo
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            
-
-
-            //add command prompt
-            //add GUI folder selection
+            //add a GUI folder selection
             string startFolder = @"C:\Users\nsc31\Desktop\DummyData";
-
-            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(startFolder);
-            IEnumerable<System.IO.FileInfo> fileList = dir.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
+            IEnumerable<System.IO.FileInfo> fileList = getDirectories(startFolder);
             
-            string searchTerm = @"RLS";
-
-            // Search the contents of each file.  
-            // A regular expression created with the RegEx class  
-            // could be used instead of the Contains method.  
-            // queryMatchingFiles is an IEnumerable<string>.  
-            var queryMatchingFiles =
-                from file in fileList
-                where file.Extension == ".pdf"
-                let fileText = GetFileText(file.FullName)
-                where fileText.Contains(searchTerm)
-                select file.FullName;
-
-            // Execute the query.  
-            Console.WriteLine("The term \"{0}\" was found in:", searchTerm);
-            foreach (string filename in queryMatchingFiles)
+            var queryTerms = new List<ISupportedQuery>() { new SocialSecurityNumber(), new CreditCardNumber() };
+            var regexTerms = new List<System.Text.RegularExpressions.Regex>();
+            foreach (var query in queryTerms)
             {
-                Console.WriteLine(filename);
+                // Create the regular expression
+                regexTerms.Add(new System.Text.RegularExpressions.Regex(query.getQueryTerm()));
             }
+            //add a GUI to select queries
+            
+            // Search the contents of each file  
+            var regexQueryMatchingFiles =
+                from file in fileList //for each file
+                let fileText = System.IO.File.ReadAllText(file.FullName)//get file text
+                from term in regexTerms//for each regex
+                let matches = term.Matches(fileText)//get matches
+                where matches.Count > 0
+                let matchedValue = from match in matches select match.Value//issue here getting the regex matched Value
+                select new { name = file.FullName, value = matchedValue};
 
-            // Keep the console window open in debug mode.  
+                foreach (var query in queryTerms)
+                {
+                    Console.WriteLine("Searched for \"{0}\"", query.getName());
+                }
+
+            if (regexQueryMatchingFiles.Any())
+            {
+                foreach (var matchedfile in regexQueryMatchingFiles)
+                {
+                    Console.WriteLine("The file \"{0}\" contained, \"{1}\". ", matchedfile.name, matchedfile.value.Count());//so here we show the number of matches instead
+                }
+            }
+            else
+            {
+                Console.WriteLine("No Matches Found");
+            }            
+
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
+
+            // write to report file
+            
         }
 
-        // Read the contents of the file.  
+        static IEnumerable<System.IO.FileInfo> getDirectories(string path)
+        {
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(path);
+
+            //will need to get all files for now
+            //add file type select GUI
+            //inspect & warn users of folder/file perm issues
+            //System.IO.SearchOption.AllDirectories gets all sub dirs as well
+            return dir.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
+        }
+
+        //expand this method for handling specialized file types
+        //expand this for ignoring files/types
+        //this impl leans heavily on .NET's built-in file reading, which is effective but naive
         static string GetFileText(string name)
         {
             string fileContents = String.Empty;
