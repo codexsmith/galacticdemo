3b6031183c866ba0a23798b5ad479b6a5c9f3bfd
Nick Smith <nsc319@gmail.com>
2022-04-11 15:09:20 -0400

    basic directory & file search copied from .Net docs, tested for txt and pdf with good test data.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//began at 2:45 PM
+//dotnet 5.0 console project
+//github 
+//starting with https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/how-to-query-the-contents-of-files-in-a-folder-lin
 
 namespace GalacticDemo
 {
@@ -6,7 +13,53 @@ namespace GalacticDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+
+            //add command prompt
+            //add GUI folder selection
+            string startFolder = @"C:\Users\nsc31\Desktop\DummyData";
+
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(startFolder);
+            IEnumerable<System.IO.FileInfo> fileList = dir.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
+            
+            string searchTerm = @"RLS";
+
+            // Search the contents of each file.  
+            // A regular expression created with the RegEx class  
+            // could be used instead of the Contains method.  
+            // queryMatchingFiles is an IEnumerable<string>.  
+            var queryMatchingFiles =
+                from file in fileList
+                where file.Extension == ".pdf"
+                let fileText = GetFileText(file.FullName)
+                where fileText.Contains(searchTerm)
+                select file.FullName;
+
+            // Execute the query.  
+            Console.WriteLine("The term \"{0}\" was found in:", searchTerm);
+            foreach (string filename in queryMatchingFiles)
+            {
+                Console.WriteLine(filename);
+            }
+
+            // Keep the console window open in debug mode.  
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        // Read the contents of the file.  
+        static string GetFileText(string name)
+        {
+            string fileContents = String.Empty;
+
+            // If the file has been deleted since we took
+            // the snapshot, ignore it and return the empty string.  
+            if (System.IO.File.Exists(name))
+            {
+                fileContents = System.IO.File.ReadAllText(name);
+            }
+            return fileContents;
         }
     }
 }
